0fcc38e721cf8283b8fb7cb6701e6493f58d1e60
Ayan <ayan.raza.2011@gmail.com>
2023-01-18 19:06:36 +0530

    6th commit

--- 8_varSizedArrays.cpp
+++ 8_varSizedArrays.cpp
@@ -7,6 +7,29 @@ using namespace std;
 
 
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int n,q,k;
+    cin>>n>>q;
+    vector<vector<int>> a(n);
+
+    for(int i = 0; i<n; i++){
+        cin>>k;
+        a[i].resize(k);
+        
+        for(int l=0; l< k; l++){
+
+            cin>>a[i][l];
+            
+
+        }
+        
+    }
+    int c,d;
+
+    for(int i = 0; i<q; i++){
+        cin>>c>>d;
+
+        cout<<a[c][d]<<endl;
+
+    }
     return 0;
 }
